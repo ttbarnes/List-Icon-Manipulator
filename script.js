b22d7f35159fd623de1c6064cbac51a135fb6d48
Tony Barnes <tony@tonybarnes.me>
2013-09-24 08:30:07 +0100

    Icon specific variables refactored

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ var config = {
 	
 	listIconType:"text", //simple text, or HTML selector (eg #myElement)
 	listIconSelector:"all",
-	listElmentIcon:"!* "
+	listElmentIconText:"!*" //the icon text (can leave empty); this should always be a CSS background image, so no HTML tag rendering required here
 }
 
 
 var listIconManipulator = {
 	init: function(){	
+		
+		//private config
+		iElmIcon = '<span class="icon">' + config.listElmentIconText + '</span>';
 		
 		//config/options
 		if (config.listIconSelector == 'all'){
@@ -16,15 +19,24 @@ var listIconManipulator = {
 			init1 = true;
 		} 
 		else {
-			//var listElmsParent = config.listIconSelector.parent();
 			var listElm = config.listIconSelector;
 		}
+		
+		if (config.listIconSelector == 'all'){
+			var listElm = 'ul';
+			
+			init1 = true;
+		} 
+		else {
+			var listElm = config.listIconSelector;
+		}
+		
 		
 		
 		//run it
 		if (init1 == true) {
 			var iElm = listElm;
-			var iElmIcon = config.listElmentIcon;
+			var iElmIconText = config.listElmentIconText;
 				
 				jQuery.each($(iElm), function() { 
 					//console.log($(this).html());

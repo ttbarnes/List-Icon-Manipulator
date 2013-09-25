6b70becfc570949b7f55ba20db728900bfad1704
Tony Barnes <tony@tonybarnes.me>
2013-09-25 18:24:09 +0100

    Initial tooltip build

--- js/listIconManipulator.alpha.1.1.1.js
+++ js/listIconManipulator.alpha.1.1.1.js
@@ -1,6 +1,8 @@
 var config = {
 	listParentSelectors:'div.listIconManipulatorExample ul:first, #customList', //choose 'all' for all UL/OL instances, or select specific parent UL/OL/DIV elements
-  listElmentIconContent:'<img src="http://www.openideas.info/wiki/images/d/d7/Template_warning.png" alt="Warning" />' //the icon content. can be simple text (eg '!*'), or HTML content
+	listElmentIconContent:'<img src="http://www.openideas.info/wiki/images/d/d7/Template_warning.png" alt="Warning" />', //the icon content. can be simple text (eg '!*'), or HTML content
+	listElementPopup:true, //popup
+	listElementPopupText:'Warning message here!' //popup text
 }
 
 var listIconManipulator = {
@@ -9,9 +11,12 @@ var listIconManipulator = {
 		//private config
 		iElmIcon = '<span class="icon">' + config.listElmentIconContent + '</span>';
 		iElmIconText = config.listElmentIconContent;
+		init1 = false;
+		init2 = false;
+		init3 = false;
 	
 		//rules
-		if (config.listParentSelectors == 'all'){
+		if (config.listParentSelectors == 'all'){ //parent list selectors
 			var iParentElm = 'ul, ol';
 			init1 = true;
 		} 
@@ -21,10 +26,40 @@ var listIconManipulator = {
 			console.log('init2 true!')
 		}
 		
+		if (config.listElementPopup == true && /\S/.test(config.listElementPopupText)) { //popup
+			var iElmPopupText = config.listElementPopupText;
+			init3 = true;
+		}
+		else {		
+			console.log('error! string is empty or contains purely whitespace. See config.listElementPopupText')
+		}
+		
 		//run it
 		var ipElm = iParentElm;
 		jQuery.each($(ipElm), function() {
+			
 			jQuery(this).addClass('listIconManipulator').children('li').wrapInner('<span class="content"></span>').prepend(iElmIcon);
+			
+			if(init3 == true) { //popup
+				jQuery(this).children('li').bind({
+					mouseenter: function testFunctionBinding (e) {
+						var iHover = jQuery(this);
+							var toolTip = jQuery('<div class="miniPopup500">BINGO - miniPopup500</div>').appendTo(this);
+							jQuery('div.miniPopup500').show();
+							jQuery(iHover).children('span.title').appendTo('div.toolTip');
+					},
+					mouseleave: function(e) {								 
+						jQuery(this).children('div.miniPopup500').remove();
+					},
+				});	
+				
+				
+			
+			}
+						
+				
+			
+			
 		});
 	}
 

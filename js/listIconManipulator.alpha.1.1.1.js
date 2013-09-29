b7c6b6ec5c18eb043f3efb25db3a730272535517
Tony Barnes <tony@tonybarnes.me>
2013-09-29 19:18:39 +0100

    JS tidy - better naming

--- js/listIconManipulator.alpha.1.1.1.js
+++ js/listIconManipulator.alpha.1.1.1.js
@@ -1,54 +1,48 @@
 var config = {
-	listParentSelectors:'div.listIconManipulatorExample ul:first, #customList', //choose 'all' for all UL/OL instances, or select specific parent UL/OL/DIV elements
-	listElmentIconContent:'<img src="http://www.openideas.info/wiki/images/d/d7/Template_warning.png" alt="Warning" />', //the icon content. can be simple text (eg '!*'), or HTML content
-	listElementPopup:true, //popup
-	listElementPopupText:'Only avaliable in the UK' //popup text
+	limParentSelectors:'div.listIconManipulatorExample ul:first, #customList', //choose 'all' for all UL/OL instances, or select specific parent UL/OL/DIV elements
+	limIconContent:'<img src="http://www.openideas.info/wiki/images/d/d7/Template_warning.png" alt="Warning" />', //the icon content. can be simple text (eg '!*'), or HTML content
+	limPopup:true, //popup
+	limPopupText:'Only avaliable in the UK' //popup text
 }
 
 var listIconManipulator = {
 	init: function(){	
 		
 		//private config
-		iElmIcon = '<span class="icon">' + config.listElmentIconContent + '</span>';
-		iElmIconText = config.listElmentIconContent;
+		limElmIcon = '<span class="icon">' + config.limIconContent + '</span>';
+		limElmIconText = config.limIconContent;
 		init1 = false;
 		init2 = false;
 		init3 = false;
 	
 		//rules
-		if (config.listParentSelectors == 'all'){ //parent list selectors
-			var iParentElm = 'ul, ol';
+		if (config.limParentSelectors == 'all'){ //parent list selectors
+			var limElmParent = 'ul, ol';
 			init1 = true;
 		} 
 		else {
-			var iParentElm = config.listParentSelectors;
+			var limElmParent = config.limParentSelectors;
 			init2 = true;
-			console.log('init2 true!')
 		}
 		
-		if (config.listElementPopup == true && /\S/.test(config.listElementPopupText)) { //popup (test if true and if the string is not empty and not just whitespace)
-			var iElmPopupText = config.listElementPopupText;
+		if (config.limPopup == true && /\S/.test(config.limPopupText)) { //popup (test if true and if the string is not empty and not just whitespace)
+			var limPopupText = config.limPopupText;
 			init3 = true;
 		}
-		else if (config.listElementPopup == false) {
-			
-		}
+		else if (config.limPopup == false) {}
 		else {		
 			console.log('error! string is empty or contains purely whitespace. See config.listElementPopupText')
 		}
 		
-		//run it
-		var ipElm = iParentElm;
-		jQuery.each($(ipElm), function() {
-			
-			jQuery(this).addClass('listIconManipulator').children('li').wrapInner('<span class="content"></span>').prepend(iElmIcon);
+		jQuery.each($(limElmParent), function() {
+			jQuery(this).addClass('listIconManipulator').children('li').wrapInner('<span class="content"></span>').prepend(limElmIcon);
 			
 			if(init3 == true) { //popup
 				jQuery(this).children('li').bind({
-					mouseenter: function testFunctionBinding (e) {
+					mouseenter: function (e) {
 						var iHover = jQuery(this);
 						iHover.addClass('active');
-						var toolTip = jQuery('<div class="miniPopup500"></div>').hide().appendTo((jQuery(this).children('span.icon'))).html(config.listElementPopupText).text();
+						var toolTip = jQuery('<div class="miniPopup500"></div>').hide().appendTo((jQuery(this).children('span.icon'))).html(config.limPopupText).text();
 						jQuery('div.miniPopup500').fadeIn(300).css({
 							'z-index':'9'
 						});

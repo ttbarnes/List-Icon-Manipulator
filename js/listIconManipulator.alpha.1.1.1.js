646635faa648983fea593c1544ac028d1581ecdb
Tony Barnes <tony@tonybarnes.me>
2013-09-30 22:33:26 +0100

    Building multiple instance rules. Instance vars added, rules/actions updated

--- js/listIconManipulator.alpha.1.1.1.js
+++ js/listIconManipulator.alpha.1.1.1.js
@@ -1,13 +1,15 @@
 var config = {
+	
 	oneInstance:{
+		yesNo:true,
 		parentSelectors:'div.listIconManipulatorExample ul:first', //choose 'all' for all UL/OL instances, or select specific parent UL/OL/DIV elements
 		iconContent:'<<', //the icon content. can be simple text (eg '!*'), or HTML content
 	},
 	twoInstances:{
-		yesNo:true,
+		yesNo:false,
 		lim1:{					
-			parentSelectors:'div.listIconManipulatorExample ul:first', //choose 'all' for all UL/OL instances, or select specific parent UL/OL/DIV elements
-			iconContent:'<img src="http://www.openideas.info/wiki/images/d/d7/Template_warning.png" alt="Warning" />', //the icon content. can be simple text (eg '!*'), or HTML content
+			parentSelectors:'div.listIconManipulatorExample ul:first', 
+			iconContent:'<img src="http://www.openideas.info/wiki/images/d/d7/Template_warning.png" alt="Warning" />',
 		},
 		lim2:{
 			parentSelectors:'#customList',
@@ -27,33 +29,46 @@ var listIconManipulator = {
 	init: function(){	
 		
 		//private config
-		limElmIcon = '<span class="icon">' + config.limIconContent + '</span>';
-		limElmIconText = config.limIconContent;
 		init1 = false;
 		init2 = false;
 		init3 = false;
 	
 		//rules
-		if (config.limParentSelectors == 'all'){ //parent list selectors
-			var limElmParent = 'ul, ol';
-			init1 = true;
-		} 
-		else {
-			var limElmParent = config.limParentSelectors;
-			init2 = true;
+		
+		
+		if (config.oneInstance.yesNo == true && /\S/.test(config.oneInstance.yesNo)) {
+			console.log('oneInstance correct')
+			instances1 = true;
+			configInstances1 = config.oneInstance;
+			console.log(instances1)
+			elmIcon = '<span class="icon">' + configInstances1.iconContent + '</span>';
+			elmIconText = configInstances1.iconContent;
 		}
 		
-		if (config.limToolTip == true && /\S/.test(config.limToolTipText)) { //popup (test if true and if the string is not empty and not just whitespace)
-			var limToolTipText = config.limToolTipText;
-			init3 = true;
-		}
-		else if (config.limToolTip == false) {}
-		else {		
-			console.log('error! string is empty or contains purely whitespace. See config.listElementPopupText')
+		if (instances1 == true){
+		  if (configInstances1.parentSelectors == 'all'){ //parent list selectors
+				var elmParent = 'ul, ol';
+				init1 = true;
+			} 
+			else {
+				var elmParent = configInstances1.parentSelectors;
+				init2 = true;
+			}
+			
+			if (config.toolTip == true && /\S/.test(config.toolTipText)) { //popup (test if true and if the string is not empty and not just whitespace)
+				var toolTipText = config.toolTipText;
+				init3 = true;
+			}
+			else if (config.toolTip == false) {}
+			else {		
+				console.log('error! string is empty or contains purely whitespace. See config.toolTipText')
+			}
+				
 		}
 		
-		jQuery.each($(limElmParent), function() {
-			jQuery(this).addClass('listIconManipulator').children('li').wrapInner('<span class="content"></span>').prepend(limElmIcon);
+		
+		jQuery.each($(elmParent), function() {
+			jQuery(this).addClass('listIconManipulator').children('li').wrapInner('<span class="content"></span>').prepend(elmIcon);
 			
 			if(init3 == true) { //toolTip
 				jQuery(this).children('li').bind({

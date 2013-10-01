a7146c51c9195a4dd7ee3687be305ad263e3fde5
Tony Barnes <tony@tonybarnes.me>
2013-10-01 08:26:04 +0100

    Building instance rules (POCing)

--- js/listIconManipulator.alpha.1.1.1.js
+++ js/listIconManipulator.alpha.1.1.1.js
@@ -3,7 +3,9 @@ var config = {
 	oneInstance:{
 		yesNo:true,
 		parentSelectors:'div.listIconManipulatorExample ul:first', //choose 'all' for all UL/OL instances, or select specific parent UL/OL/DIV elements
-		iconContent:'<<', //the icon content. can be simple text (eg '!*'), or HTML content
+		iconContent:'>>', //the icon content. can be simple text (eg '!*'), or HTML content
+		toolTip:true,
+		toolTipText:'Be careful!'
 	},
 	twoInstances:{
 		yesNo:false,
@@ -16,8 +18,8 @@ var config = {
 			iconContent:'!-WARNING-!',
 		}
 	},
-	toolTip:true, //toolTip
-	toolTipText:'Only avaliable in the UK' //toolTip text
+	//toolTip:true, //toolTip
+	//toolTipText:'Only avaliable in the UK' //toolTip text
 	
 	//limParentSelectors:'div.listIconManipulatorExample ul:first, #customList', //choose 'all' for all UL/OL instances, or select specific parent UL/OL/DIV elements
 	//limIconContent:'<img src="http://www.openideas.info/wiki/images/d/d7/Template_warning.png" alt="Warning" />', //the icon content. can be simple text (eg '!*'), or HTML content
@@ -31,7 +33,7 @@ var listIconManipulator = {
 		//private config
 		init1 = false;
 		init2 = false;
-		init3 = false;
+		//initTooltip = true;
 	
 		//rules
 		
@@ -41,8 +43,8 @@ var listIconManipulator = {
 			instances1 = true;
 			configInstances1 = config.oneInstance;
 			console.log(instances1)
-			elmIcon = '<span class="icon">' + configInstances1.iconContent + '</span>';
 			elmIconText = configInstances1.iconContent;
+			elmIcon = '<span class="icon">' + configInstances1.iconContent + '</span>';
 		}
 		
 		if (instances1 == true){
@@ -55,37 +57,40 @@ var listIconManipulator = {
 				init2 = true;
 			}
 			
-			if (config.toolTip == true && /\S/.test(config.toolTipText)) { //popup (test if true and if the string is not empty and not just whitespace)
-				var toolTipText = config.toolTipText;
-				init3 = true;
+			if (configInstances1.toolTip == true && /\S/.test(configInstances1.toolTipText)) { //popup (test if true and if the string is not empty and not just whitespace)
+				initTooltip = true;
 			}
-			else if (config.toolTip == false) {}
-			else {		
+			else if (configInstances1.toolTip == false) {}
+			/*else {		
 				console.log('error! string is empty or contains purely whitespace. See config.toolTipText')
-			}
+			}*/
+			
+			jQuery.each($(elmParent), function() {
+				jQuery(this).addClass('listIconManipulator').children('li').wrapInner('<span class="content"></span>').prepend(elmIcon);
+				
+				if(configInstances1.toolTip == true) { //toolTip
+				console.log('instance 1 tooltip true')
+				
+					jQuery(this).children('li').bind({
+						mouseenter: function (e) {
+				console.log('hovered')
+							var iHover = jQuery(this);
+							iHover.addClass('active');
+							var limToolTip = jQuery('<div class="limToolTip"></div>').hide().appendTo((jQuery(this).children('span.icon'))).html(configInstances1.toolTipText).text();
+							jQuery('div.limToolTip').fadeIn(300).css({
+								'z-index':'9'
+							});
+						},
+						mouseleave: function(e) {
+							jQuery(this).removeClass('active').children('span.icon').children('div.limToolTip').fadeOut(200).remove();
+						},
+					});
+				}
+			});
+			
+			
 				
 		}
-		
-		
-		jQuery.each($(elmParent), function() {
-			jQuery(this).addClass('listIconManipulator').children('li').wrapInner('<span class="content"></span>').prepend(elmIcon);
-			
-			if(init3 == true) { //toolTip
-				jQuery(this).children('li').bind({
-					mouseenter: function (e) {
-						var iHover = jQuery(this);
-						iHover.addClass('active');
-						var limToolTip = jQuery('<div class="limToolTip"></div>').hide().appendTo((jQuery(this).children('span.icon'))).html(config.limToolTipText).text();
-						jQuery('div.limToolTip').fadeIn(300).css({
-							'z-index':'9'
-						});
-					},
-					mouseleave: function(e) {
-						jQuery(this).removeClass('active').children('span.icon').children('div.limToolTip').fadeOut(200).remove();
-					},
-				});
-			}
-		});
 	}
 }
 listIconManipulator.init();
